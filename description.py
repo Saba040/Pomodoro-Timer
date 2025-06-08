d504feec25188effe2ece50a84a3d520f2a08f38
Saba040 <bediashvilisaba04@gmail.com>
2025-06-08 15:21:35 +0400

    description

---
+++ description.py
@@ -0,0 +1,16 @@
+# ეს არის PyQt5-ზე აგებული Pomodoro ტაიმერის გრაფიკული
+# აპლიკაცია, რომელიც დაგეხმარება დროის მართვასა და კონცენტრაციაში.
+# პროგრამა იყენებს 25 წუთიან სამუშაო და 5 წუთიან შესვენების ციკლს.
+# აპლიკაცია ავტომატურად გაწვდით შეტყობინებას სამუშაო დროის
+# დასრულებისას და იწყებს შესვენების ტაიმერს. სურვილის შემთხვევაში
+# შესაძლებელია შესვენების დროს მუსიკის გაჟღერებაც.
+
+
+
+# This is a graphical Pomodoro Timer application built
+# with PyQt5, designed to help with time management and
+# focus. The app follows the Pomodoro technique with
+# 25-minute work sessions followed by 5-minute breaks.
+# It automatically notifies the user when a session ends
+# and starts the break timer. Optionally, relaxing music
+# can be played during breaks for a better experience.
